c70913ab984b70fd75716d1755f30a41bd3377a6
nasdn0 <fukumeanfukumean21@gmail.com>
2025-12-16 19:21:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelectorAll('body');
+
+buttons.forEach(function(button) {
+    console.log(button);
+    button.addEventListener('click', function(e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey') {body.style.backgroundColor = e.targetrt.id;}
+        if (e.target.id === 'white') {body.style.backgroundColor = e.targetrt.id;}
+        if (e.target.id === 'blue') {body.style.backgroundColor = e.targetrt.id;}
+        if (e.target.id === 'yellow') {body.style.backgroundColor = e.targetrt.id;}
+
+});
